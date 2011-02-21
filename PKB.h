94a067d1ed4ce5d8cd8634c07f3e734e242f1b61
zqkun520 <zqkun520@513be519-b518-b7cc-09c9-68eb53d926ad>
2011-02-21 08:15:13 +0000

    updated PKB.h

--- PKB.h
+++ PKB.h
@@ -1,28 +1,33 @@
-#include "Node.h"
 #include "AST.h"
 #include "VarTable.h"
 #include "ProcTable.h"
-
+#include "ConstantTable.h"
 //#include "ModifyTable.h"
 //#include "UseTable.h"
-#include "ConstantTable.h"
+
 
 class PKB
 {
 public:
 	/**
+	 * PKB Part
+	 */
+	void pkb_AddToASTList(AST* ast, int procIndex);
+	AST* getASTByProcdureIndex(int procIndex);
+	int getProcedureIndexByAST(AST* ast);
+	/**
 	 * AST Part
 	 */
-	Node*		node_CreateNode(Node::NodeType nt, int statNum, int name);
-	list<int>	node_GetChild(int stmtNum);
-	Node*		node_GetPreviousStatement(Node* d);
-	Node*		node_GetFollowingStatement(Node* d);
-	Node*		node_GetNodeByStatementNum(int index);
-	void ast_AddChild(Node *d, Node *childNode);
-	void ast_AddDown(Node *upperNode, Node *bottomNode);
-	void ast_AddFollow(Node *d, Node *followNode);
-	void ast_AddProcedure(Node *d);
-	AST* ast_CreateASTnode();
+	AST*		ast_CreateASTnode();
+	Node*		ast_CreateNode(AST* ast, Node::NodeType nt, int statNum, int name);
+	list<int>	ast_GetChild(AST* ast, int stmtNum);
+	Node*		ast_GetPreviousStatement(AST* ast, Node* d);
+	Node*		ast_GetFollowingStatement(AST* ast, Node* d);
+	Node*		ast_GetNodeByStatementNum(AST* ast, int index);
+	void		ast_AddChild(AST* ast, Node *d, Node *childNode);
+	void		ast_AddDown(AST* ast, Node *upperNode, Node *bottomNode);
+	void		ast_AddFollow(AST* ast, Node *d, Node *followNode);
+	void		ast_AddProcedure(AST* ast, Node *d);
 	/**
 	 * VarTable Part
 	 */
@@ -51,7 +56,7 @@ public:
 
 private:
 	Node*			node;
-	vector<AST*>	astList;
+	map<int, AST*>	astList;
 	VarTable*		varTable;
 	ProcTable*		procTable;
 	/*
