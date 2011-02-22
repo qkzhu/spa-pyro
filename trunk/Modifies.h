a62d502fbd14966d4a62ee0ff2b8eb9f789ab612
speedperry <speedperry@513be519-b518-b7cc-09c9-68eb53d926ad>
2011-02-22 07:06:14 +0000

    Modify Table uploaded. In the midst of testing the methods

---
+++ trunk/Modifies.h
@@ -0,0 +1,21 @@
+#include <set>
+#include <string>
+#include <map>
+using namespace std;
+
+class Modifies
+{
+public:
+	set<int> getModifiedVar(int); //takes in a statNum; return varIndex
+	set<int> getModifiedVarPI(int); // takes in a procIndex; return varIndex
+	set<int> getStmtModifies(int); // takes in a varIndex
+	set<int> getProcModifies(int); // takes in a varIndex; return procIndex
+	void setModify(int, int); //stmtNum, varIndex
+	void setModifyPV(int, int); // procIndex, varIndex
+private:
+	
+	map<int, set<int>> mStmtModMap; // map each stmt# to a set of modified varIndex
+	map<int, set<int>> mProcModMap; // map each ProcIndex to a set of modified varIndex
+	map<int, set<int>> mVarStmtMap; // map each modified varIndex to a set of stmt#
+	map<int, set<int>> mVarProcMap; // map each modified varIndex to a set of procIndex
+};
