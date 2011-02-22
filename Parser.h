3e401af6f8ec9d97da018838e0a61430ab62bcbc
wuliangx <wuliangx@513be519-b518-b7cc-09c9-68eb53d926ad>
2011-02-22 10:21:20 +0000

    Modify and Use updating not added yet.

--- Parser.h
+++ Parser.h
@@ -11,7 +11,7 @@ using namespace std;
 class Parser 
 {
 public:
-	Parser(VarTable &varTable, AST &ast, std::string filename);
+	Parser(PKB &pkb, std::string filename);
 	void parseProgram();
 	void loadFile(std::string filename); //loads a new source file
 	~Parser();
@@ -19,18 +19,24 @@ public:
 private:
 	int mLineNum;
 	int mStatNum;
-	VarTable &mVarTable;
-	AST &mAst;
+	int mCurrProcIndex; //tracks the current procedure being parsed.
 
-	string getToken(); 	//extracts next token, returns empty string if none found.
+	//functions to hold pkb instances
+	PKB &mPkb;
+	AST *mAst;
+
+	//functions for tokenizing
+	string getToken();		//extracts next token, returns empty string if none found.
 	string peekToken();	//peeks next token, empty string if none found
-	bool hasToken();	//checks whether there are more tokens to be read
-	void skipSpaces();	//Skips all spaces until the next non-space char is encountered
+	bool hasToken();		//checks whether there are more tokens to be read
+	void skipSpaces();		//Skips all spaces until the next non-space char is encountered
 
+	//functions that print error msgs
 	void error(std::string expected, std::string received);	//prints the error msg and exits
 	void match(std::string token);							//
 
-	Node *parseProcedure();
+	//functions to parse the source program
+	void parseProcedure();
 	Node *parseStmtList(Node* parentNode);
 	Node *parseStmt();
 	Node *parseAssignment();
@@ -38,22 +44,26 @@ private:
 	Node *parseIf();
 	Node *parseCall();
 
+	//functions to check whether tokens are constants or delimiters
 	bool isConstant(string tok);
 	bool isDelimiter(char tok);
 
-	//for parsing trees.
+	//for parsing expression trees e.g. (1+2) * 3.
 	bool isBracket(string s);
 	bool isOperator(string s);
 	int getPriority(string s);
 	void addOperator(vector<Node*> &tree, string op);
 
+	//functions to check validity
 	bool checkVariableExists(std::string var_name);
 	void checkValidName(std::string var_name);
 	bool isValidName(std::string var_name);
 	void checkValidFile();
 
+	//member variables to hold files
 	string mFilename;
 	ifstream *mpFile;
+
 	stringstream *mStringBuf;
 
 	static const string mKeyWords[];
