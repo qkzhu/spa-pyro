82e605b7aee72c622dcdd2e2231b9f5c85cb2ad5
speedperry <speedperry@513be519-b518-b7cc-09c9-68eb53d926ad>
2011-02-22 05:34:18 +0000

    Deleted ProcTable.h and VarTable.h. VarTable.cpp and ProcTable.cpp directly call SymbolTable.h

--- SymbolTable.h
+++ SymbolTable.h
@@ -20,3 +20,25 @@ public:
 private:
 	map<string, int> mSymbolMap;
 };
+
+class VarTable : public SymbolTable
+{
+public:
+	void insertVar(string);
+	string getVarName(int);
+	int getVarIndex(string);
+	int getSize(); 
+	bool containsVar(string);
+	set<string> getAllVar();
+};
+
+class ProcTable : public SymbolTable
+{
+public:
+	void insertProc(string);
+	string getProcName(int);
+	int getProcIndex(string);
+	int getSize(); 
+	bool containsProc(string);
+	set<string> getAllProc();
+};
