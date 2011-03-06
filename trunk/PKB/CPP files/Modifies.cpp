7b04b556b6b23591aa1549e0815b560662e9b274
speedperry <speedperry@513be519-b518-b7cc-09c9-68eb53d926ad>
2011-03-06 07:47:26 +0000

    if returning a empty set,
    1st element of set is assigned -1;

--- trunk/PKB/CPP files/Modifies.cpp
+++ trunk/PKB/CPP files/Modifies.cpp
@@ -1,5 +1,4 @@
 #include "Modifies.h"
-//#include <iostream>
 using namespace std;
 
 set<int> Modifies::getModifiedVar(int stmtNum)
@@ -7,7 +6,8 @@ set<int> Modifies::getModifiedVar(int stmtNum)
 	set<int> varIndexes; // empty set of varIndexes
 	if(mStmtModMap.find(stmtNum) != mStmtModMap.end())
 		return mStmtModMap[stmtNum];
-	return varIndexes; // return empty set of varIndexes if the enquired stmtNum has no modified variable
+	varIndexes.insert(-1);
+	return varIndexes; //return -1 if set does not contain any varIndex
 }// end getModifiedVar
 
 set<int> Modifies::getModifiedVarPI(int procIndex)
@@ -17,7 +17,8 @@ set<int> Modifies::getModifiedVarPI(int procIndex)
 	{
 		return mProcModMap[procIndex];
 	}
-	return varIndexes; // return empty set of varIndexes if the enquired procIndex has no modified variable
+	varIndexes.insert(-1);
+	return varIndexes; //return -1 if set does not contain any varIndex
 }// end getModifiedVarPI
 
 set<int> Modifies::getStmtModifies(int varIndex)
@@ -27,7 +28,8 @@ set<int> Modifies::getStmtModifies(int varIndex)
 	{
 		return mVarStmtMap[varIndex];
 	}
-	return stmts; // return empty set of stmst if the enquired varIndex has no stmts modifying it.
+	stmts.insert(-1);
+	return stmts; //return -1 if set does not contain any stmts
 }// end getStmtModifies
 
 set<int> Modifies::getProcModifies(int varIndex)
@@ -37,7 +39,8 @@ set<int> Modifies::getProcModifies(int varIndex)
 	{
 		return mVarProcMap[varIndex];
 	}
-	return procIndexes; // return empty set of procIndexes if the enquired varIndex has no procIndex modiftying it
+	procIndexes.insert(-1);
+	return procIndexes; //return -1 if set does not contain any procIndex
 }// end getProcModifies
 
 void Modifies::setModify(int stmtNum, int varIndex)
@@ -64,39 +67,3 @@ void Modifies::setModifyPV(int procIndex, int varIndex)
 	//update mVarProcMap. Mapping each variables to a set of procedures that modifies it.
 	mVarProcMap[varIndex].insert(procIndex);
 }// end setModifyPV
-
-/******Driver program*******
-int main()
-{
-	Modifies M;
-	int stmt = 1;
-	for(stmt = 1; stmt <= 5; stmt++)
-	{
-		if(stmt == 5)
-			M.setModify(5, 12);
-		else
-			M.setModify(stmt, 223); // 5 procedures modified varIndex = 223
-		
-	}
-	M.setModify(1, 2); //error message to occur
-	cout << "Stmt1 modifies varIndex = ";
-	set<int> test1 = M.getModifiedVar(1);
-	for(set<int>::iterator it = test1.begin(); it != test1.end(); it++)
-	{
-		cout << *it << " ";
-	}
-	cout << endl;
-	cout << "Stmt2 modifies varIndex = " << *M.getModifiedVar(2).begin() << endl;
-	cout << "Stmt5 modifies varIndex = " << *M.getModifiedVar(5).begin() << endl;
-	cout << "Size of stmts that modifies varIndex = 223 : " << M.getStmtModifies(223).size() << endl;
-	cout << "varIndex = 223 is modified by stmt#: ";
-	set<int> test = M.getStmtModifies(223);
-	for(set<int>::iterator it = test.begin(); it != test.end(); it++)
-	{
-		cout << *it << " ";
-	}
-	cout << endl;
-	//cout << "VarIndex = 223 is modified by procedures: " << M.getProcModifies(223) << endl;
-	string readin;
-	cin >> readin;
-}*/
