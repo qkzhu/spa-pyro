740e477655d69f3b995108245acf03d83e0dafe8
zqkun520 <zqkun520@513be519-b518-b7cc-09c9-68eb53d926ad>
2011-03-15 11:17:03 +0000

    new CFG

--- trunk/PKB/Header files/CFG.h
+++ trunk/PKB/Header files/CFG.h
@@ -2,21 +2,24 @@
 #define CFG_H
 
 #include "CFGNode.h"
+#include <map>
 
 using namespace std;
 
 class CFG
 {
 public:
-	CFGNode* addCFGNode(int stmtNum);
-	CFGNode* addCFGNode(vector<int> &stmtNumList);
-	void addProcToCFGNodeMap(int procIndex, CFGNode* rootNode);
+
+	void addNext(int stmtNum, int stmtNum);
+	void addProcToCFGNodeMap(int procIndex, int stmtNum);
 
 	void getNext(vector<int>& theNext, int stmt);		//(for different nodes, insert a -1 inside.)
 	void getNextUp(vector<int>& theNextUp, int stmt);	//(for different nodes, insert a -1 inside.)
 
-private: 
-	map<int procIndex, CFGNode* rootNode>	procToCFGNodeMap;
+private:
+	map<int, int> procToCFGNodeMap;//procIndex, stmtNum
+	map<int, set<int>> forwardMap;	
+	map<int, set<int>> reverseMap;
 };
 
 
