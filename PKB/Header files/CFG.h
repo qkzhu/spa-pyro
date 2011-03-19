faf90d05cd11c268c37258c671f22ea12b1bb8c7
zqkun520 <zqkun520@513be519-b518-b7cc-09c9-68eb53d926ad>
2011-03-19 09:39:49 +0000

    minor changes

--- PKB/Header files/CFG.h
+++ PKB/Header files/CFG.h
@@ -17,6 +17,11 @@ public:
 	void getNext(vector<int>& theNext, int stmt);		//(for different nodes, insert a -1 inside.)
 	void getNextUp(vector<int>& theNextUp, int stmt);	//(for different nodes, insert a -1 inside.)
 
+	//For debugging
+	map<int, set<int>> CFG::getForwardMap();
+	map<int, set<int>> CFG::getReverseMap();
+	void CFG::prtMap(map<int, set<int>> &currMap);
+
 private:
 	void CFG::getNextLink(vector<int>& theNext, int stmt, map<int, set<int> >& theMap);
 	void processStatement(Node *inputNode, Node *parentNode, Node* followingNode);
